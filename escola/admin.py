392568613faeb685a5dfd0ade7645345ba3a8ca8
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2024-03-22 13:04:51 -0300

    creating the serializer and the authentication

--- escola/admin.py
+++ escola/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from escola.models import Aluno, Curso
+from escola.models import Aluno, Curso, Matricula
 # Register your models here.
 class AlunosAdmin(admin.ModelAdmin):
     list_display = ('id', 'nome', 'rg', 'cpf', 'data_nascimento')
@@ -16,3 +16,11 @@ class CursosAdmin(admin.ModelAdmin):
     list_per_page = 20
 
 admin.site.register(Curso, CursosAdmin)
+
+class MatriculasAdmin(admin.ModelAdmin):
+    list_display = ('id', 'aluno', 'curso', 'periodo')
+    list_display_links = ('id',)
+    search_fields = ('aluno',)
+    list_per_page = 20
+
+admin.site.register(Matricula, MatriculasAdmin)
